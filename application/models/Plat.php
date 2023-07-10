435762858f5754097ea80f5a34ae5ae5b88cd5e9
Tojonirina Mandresy RANDRIANASOLO <114937300+Ericka2306@users.noreply.github.com>
2023-07-11 00:49:21 +0300

    crud

--- application/models/Plat.php
+++ application/models/Plat.php
@@ -5,10 +5,29 @@
             $this->load->database();
         }
         
+        public function create_plat($data) {
+            $this->db->insert('plat', $data);
+            return $this->db->insert_id();
+        }
+
         public function select_plat() {
             $sql="SELECT * FROM plat";
             $query = $this->db->query($sql);
             return $query->result();
+        }
+        public function get_plat($id) {
+            $query = $this->db->get_where('plat', array('id' => $id));
+            return $query->row();
+        }
+
+        public function update_plat($id_plat, $nom, $sary){
+            $data = array(
+                'nom' => $nom,
+                'sary' => $sary,
+                
+            );
+            $this->db->where('id', $id_plat);
+            $this->db->update('plat', $data);
         }
     }
 ?>
---
+++ application/views/liste_exercice.php
@@ -0,0 +1,21 @@
+<div class="container mt-5">
+    <h2>Liste des exercices</h2>
+    <div class="row">
+        <?php foreach ($exercice_data as $exercice) { ?>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="chemin_vers_l_image" class="card-img-top" alt="Image du exercice">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $exercice->nom; ?></h5>
+                        <p class="card-text">Durée : <?php echo $exercice->sary; ?></p>
+                        <a href="<?php echo base_url('ControllerBack/modifier_exercice/'.$exercice->id); ?>" class="btn btn-primary">modifier</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    <a href="<?php echo base_url('ControllerBack/create_exercice'); ?>">Ajouter un Exercice</a>
+
+</
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
