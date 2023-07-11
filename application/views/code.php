1eb8fb28d303ce4fa1c28d7f7f8bcf628a7a9f18
Steel Soul <120133470+P15B-Y32-ETU1951@users.noreply.github.com>
2023-07-11 13:28:22 +0300

    Code_confirm

--- application/views/code.php
+++ application/views/code.php
@@ -1,4 +1,5 @@
 <form action="<?php echo site_url('Control_code/reserver')?>" method="post" >
+<h3>Porte monnaie:<?php echo $utilisateur->caisse ?></h3>
  <label for="code">Choisir un code :</label>
     <select name="code" id="code">
         <?php foreach ($code as $code): ?>
