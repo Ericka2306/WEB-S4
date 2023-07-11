19b98014d33c26b3a8b2c2522e9280ce404878cb
Steel Soul <120133470+P15B-Y32-ETU1951@users.noreply.github.com>
2023-07-11 10:27:29 +0300

    atao

--- application/models/sport.php
+++ application/models/sport.php
@@ -1,14 +1,39 @@
-<?php 
-    class sport extends CI_Model{
-        public function __construct(){
+<?php
+    class Sport extends CI_Model {
+        public function __construct() {
             parent::__construct();
             $this->load->database();
-
         }
-        public function select_sport(){
+        // Create
+        public function create_sport($data) {
+            $this->db->insert('sport', $data);
+            return $this->db->insert_id();
+        }
+
+        public function get_sport($id) {
+            $query = $this->db->get_where('sport', array('id' => $id));
+            return $query->row();
+        }
+
+        public function select_sport() {
             $sql="SELECT * FROM sport";
-            $query=$this->db->query($sql);
+            $query = $this->db->query($sql);
             return $query->result();
+        }
+
+        public function update_sport($id_sport, $nom, $duree){
+            $data = array(
+                'nom' => $nom,
+                'duree' => $duree
+            );
+            $this->db->where('id', $id_sport);
+            $this->db->update('sport', $data);
+        }
+
+        // Delete
+        public function delete_sport($id) {
+            $this->db->where('id', $id);
+            $this->db->delete('sport');
         }
     }
 ?>
